3cc30448ba4df9efa942cf66f78468da5d1ae5b1
valleyman89 <dev@stevenrolph.com>
2022-06-03 10:40:40 +0100

    define filterresult component

---
+++ src/components/FilterResult.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { useFilterContext } from "../context/FilterContext";
+
+const FilterResult = () => {
+  const { filter, setFilter, deleteFilterItem } = useFilterContext();
+  return (
+    <div className="filter shadow">
+      {filter.map((skillName, index) => {
+        return (
+          <div key={index} className="tablet">
+            <div className="skill-name">{skillName}</div>
+            <div
+              className="kill-skill pointer"
+              onClick={() => deleteFilterItem(skillName)}
+            >
+              <button
+                onClick={() => deleteFilterItem(skillName)}
+                className="close"
+              >
+                <img aria-label="remove" src="../images/icon-remove.svg" />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <button className="clear pointer" onClick={() => setFilter([])}>
+        clear
+      </button>
+    </div>
+  );
+};
+
+export default FilterResult;
