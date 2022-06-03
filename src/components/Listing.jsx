008d90bcbcd04a8347d09e4942f44973bb975f45
valleyman89 <dev@stevenrolph.com>
2022-06-03 10:41:00 +0100

    tidy listing component

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,37 +1,31 @@
 import React from "react";
 import Badge from "./Badge";
+import FilterButton from "./FilterButton";
 
 const Listing = ({ data }) => {
   return (
-    <li className="listing">
-      {/* <img className="logo" src={data.logo} /> */}
-      {data.id} {data.position}
-      <Badge fresh={data.new} featured={data.featured} />
-      <ul className="tools">
-        {data.tools &&
-          data.tools.map((tool, i) => (
-            <li key={i}>
-              <button>{tool}</button> (tool)
-            </li>
-          ))}
-      </ul>
-      <ul className="languges">
-        {data.languages &&
-          data.languages.map((language, i) => (
-            <li key={i}>
-              <button
-                onClick={(e) => {
-                  console.log(e.target.name);
-                }}
-                name={language}
-              >
-                {language}
-              </button>{" "}
-              (language)
-            </li>
-          ))}
-      </ul>
-    </li>
+    <div className="listing shadow">
+      <div className="logo">
+        <img src={data.logo} />
+      </div>
+      <div className="name">
+        {data.company} <Badge fresh={data.new} featured={data.featured} />
+      </div>
+      <div className="role"> {data.position}</div>
+      <div className="details">
+        {data.postedAt} • {data.contract} • {data.location}
+      </div>
+      <div className="skills">
+        {data.tools.map((tool, index) => (
+          <FilterButton key={index} skill={tool} />
+        ))}
+        {data.languages.map((language, index) => (
+          <FilterButton key={index} skill={language} />
+        ))}
+        <FilterButton skill={data.role} />
+        <FilterButton skill={data.level} />
+      </div>
+    </div>
   );
 };
 
