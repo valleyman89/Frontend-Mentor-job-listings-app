003d83c0579d956728fbc951962682a93da94363
valleyman89 <dev@stevenrolph.com>
2022-06-07 00:30:31 +0100

    add white class

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Badge = ({ featured, fresh }) => {
   return (
     <>
-      {fresh ? <span className="badge fresh">new!</span> : null}
-      {featured ? <span className="badge featured">featured</span> : null}
+      {fresh ? <span className="badge fresh white">new!</span> : null}
+      {featured ? <span className="badge featured white">featured</span> : null}
     </>
   );
 };
